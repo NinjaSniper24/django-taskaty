5ff71353544b52e04f0c3a1197ae85370c759122
Saif <sayfyassinetouil04@gmail.com>
2024-02-21 11:35:46 +0100

    Models completed

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.conf.global_settings import AUTH_USER_MODEL
 
-# Create your models here.
+class Categoty(models.Model) :
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+    
+class Status(models.IntegerChoices):
+    PENDING = 1 , 'Pending'
+    COMPLETED = 2 , 'Completed'
+    POSTPONED = 3 , 'Postponed'
+    CANDELED = 4 , 'Canceled'
+
+class Project(models.Model) :
+    title = models.CharField( max_length=50)
+    description = models.TextField()
+    status = models.IntegerField(choices = Status.choices, default = Status.PENDING)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    categoty = models.ForeignKey(Categoty, on_delete=models.PROTECT)
+    user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self) :
+        return self.title
+    
+class Task(models.Model) :
+    description = models.TextField()
+    is_completed = models.BooleanField(default=False)
+    status = models.IntegerField(choices = Status.choices, default = Status.PENDING)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.description
